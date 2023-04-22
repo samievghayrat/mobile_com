3bd17d0f8a551bdb6363cb08ce5fd8991cee7e86
ghayratsamiev <Ghayrat Samiev>
2023-04-22 15:34:24 +0900

    Initial commit

---
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ghayrat_samiev.mobile_com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
