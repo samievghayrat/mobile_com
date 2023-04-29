77498fa6d85a349e4c19afee6ed99ea69a788334
ghayratsamiev <Ghayrat Samiev>
2023-04-29 14:54:47 +0900

    simple calculator app

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ghayrat_samiev.mobile_com;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //hello world
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText editText = findViewById(R.id.editTextNumber);
+        TextView textViewCorrectAnswer = findViewById(R.id.correctAnswer);
+        TextView textViewIncorrect = findViewById(R.id.textViewIncorrectAnswer);
+        View answerButton = findViewById(R.id.buttonAnswer);
+
+
+        answerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String text = editText.getText().toString();
+                int number = Integer.parseInt(text);
+                if(number == 15){
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrect.setVisibility(View.GONE);
+
+                }else{
+                    textViewCorrectAnswer.setVisibility(View.GONE);
+                    textViewIncorrect.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
