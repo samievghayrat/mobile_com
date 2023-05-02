b37a987f1ce4d0ace7c374e135d0b7024f7b0aa6
ghayratsamiev <Ghayrat Samiev>
2023-05-02 20:22:25 +0900

    changed the button width

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -1,7 +1,5 @@
 package com.ghayrat_samiev.mobile_com;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
@@ -21,28 +19,6 @@ public class MainActivity extends AppCompatActivity {
         dialog.setContentView(R.layout.pop_ip_dialog_screen);
         dialog.setCancelable(true); //Optional
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
-//
-//        View Okay = dialog.findViewById(R.id.btn_okay);
-//        View Cancel = dialog.findViewById(R.id.btn_cancel);
-
-//        Okay.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Toast.makeText(MainActivity.this, "Okay", Toast.LENGTH_SHORT).show();
-//                dialog.dismiss();
-//            }
-//        });
-
-//        Cancel.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
-//                dialog.dismiss();
-//            }
-//        });
-
 
         ShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
