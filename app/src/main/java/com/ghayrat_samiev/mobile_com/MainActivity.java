3b67413929d5db4b8c7301de622cf9f27bbfb811
ghayratsamiev <Ghayrat Samiev>
2023-05-02 17:37:51 +0900

    fixing popup Dialog

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -21,27 +21,27 @@ public class MainActivity extends AppCompatActivity {
         dialog.setContentView(R.layout.pop_ip_dialog_screen);
         dialog.setCancelable(true); //Optional
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
+//
+//        View Okay = dialog.findViewById(R.id.btn_okay);
+//        View Cancel = dialog.findViewById(R.id.btn_cancel);
 
-        View Okay = dialog.findViewById(R.id.btn_okay);
-        View Cancel = dialog.findViewById(R.id.btn_cancel);
+//        Okay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast.makeText(MainActivity.this, "Okay", Toast.LENGTH_SHORT).show();
+//                dialog.dismiss();
+//            }
+//        });
 
-        Okay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(MainActivity.this, "Okay", Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
-
-        Cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
+//        Cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+//                dialog.dismiss();
+//            }
+//        });
 
 
         ShowDialog.setOnClickListener(new View.OnClickListener() {
