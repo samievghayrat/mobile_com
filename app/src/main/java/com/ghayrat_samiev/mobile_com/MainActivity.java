a3c047ca982682e1994634f2fc83f6f9b453641d
ghayratsamiev <Ghayrat Samiev>
2023-05-02 23:02:54 +0900

    image picker working yessss

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -1,10 +1,10 @@
 package com.ghayrat_samiev.mobile_com;
 
 import android.app.Dialog;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
@@ -15,33 +15,40 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private Dialog dialog;
     private View ShowDialog;
+    ImageView image_view;
 
 
-    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
-            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
-                // Callback is invoked after the user selects a media item or closes the
-                // photo picker.
-                if (uri != null) {
-                    Log.d("PhotoPicker", "Selected URI: " + uri);
-                } else {
-                    Log.d("PhotoPicker", "No media selected");
-                }
-            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Image image;
+
+        image_view = findViewById(R.id.imageView);
         View imagePicker_button = findViewById(R.id.upload_map_button);
 
+        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    // Callback is invoked after the user selects a media item or closes the
+                    // photo picker.
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                        image_view.setImageURI(uri);
+                        image_view.setVisibility(View.VISIBLE);
+
+                    } else {
+                        Log.d("PhotoPicker", "No media selected");
+                    }
+                });
         imagePicker_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 pickMedia.launch(new PickVisualMediaRequest.Builder()
                         .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                         .build());
+
             }
         });
+
 
         ShowDialog = findViewById(R.id.wardriving_button);
         dialog = new Dialog(this);
