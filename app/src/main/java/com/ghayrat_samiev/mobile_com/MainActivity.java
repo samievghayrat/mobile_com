0edfba765095fdd8cccc42a891e32e777901b944
ghayratsamiev <Ghayrat Samiev>
2023-05-02 21:58:51 +0900

    image picker instance with strange error

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -1,30 +1,53 @@
 package com.ghayrat_samiev.mobile_com;
 
+import android.app.Dialog;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.os.Bundle;
-import android.view.View;
-import  android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Dialog dialog;
     private View ShowDialog;
 
+
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: " + uri);
+                } else {
+                    Log.d("PhotoPicker", "No media selected");
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Image image;
+        View imagePicker_button = findViewById(R.id.upload_map_button);
+
+        imagePicker_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
+            }
+        });
 
         ShowDialog = findViewById(R.id.wardriving_button);
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.pop_ip_dialog_screen);
         dialog.setCancelable(true); //Optional
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
-
-
-
-
 
         ShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show(); // Showing the dialog here
             }
         });
-
 
     }
 }
