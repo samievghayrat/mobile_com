422259e77eb1ae86310f154cae8416aca0ef1066
ghayratsamiev <Ghayrat Samiev>
2023-05-02 23:20:32 +0900

    added visibility after uploading and deleting an image

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -11,11 +11,13 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
     private Dialog dialog;
     private View ShowDialog;
     ImageView image_view;
+    View delete_button;
+    View _wardriving_button;
+    View localization;
 
 
     @Override
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         image_view = findViewById(R.id.imageView);
         View imagePicker_button = findViewById(R.id.upload_map_button);
+        delete_button = findViewById(R.id.delete_button);
+        _wardriving_button = findViewById(R.id.wardriving_button);
+        localization = findViewById(R.id.localization_button);
 
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
                 registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("PhotoPicker", "Selected URI: " + uri);
                         image_view.setImageURI(uri);
                         image_view.setVisibility(View.VISIBLE);
+
+
 
                     } else {
                         Log.d("PhotoPicker", "No media selected");
@@ -45,6 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 pickMedia.launch(new PickVisualMediaRequest.Builder()
                         .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                         .build());
+                imagePicker_button.setVisibility(View.GONE);
+                _wardriving_button.setVisibility(View.VISIBLE);
+                delete_button.setVisibility(View.VISIBLE);
+                localization.setVisibility(View.VISIBLE);
+
+
+
+
+            }
+        });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image_view.setVisibility(View.GONE);
+                imagePicker_button.setVisibility(View.VISIBLE);
+                _wardriving_button.setVisibility(View.GONE);
+                delete_button.setVisibility(View.GONE);
+                localization.setVisibility(View.GONE);
 
             }
         });
