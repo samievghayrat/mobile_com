77579fc2a5a881c8fbe982d711d621ab9f036081
ghayratsamiev <Ghayrat Samiev>
2023-05-13 05:49:21 +0900

    added readme

--- app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/MainActivity.java
@@ -1,28 +1,39 @@
 package com.ghayrat_samiev.mobile_com;
+
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.ghayrat_samiev.mobile_com.R.layout.would_you_like_to_scan;
+
 public class MainActivity extends AppCompatActivity {
-    private Dialog dialog;
-    private View ShowDialog;
-    ImageView image_view;
+    //private  Dialog wold_you_lie_to_scan;
+    //buttons
+    ImageButton image_view;
     View delete_button;
     View _wardriving_button;
     View localization;
 
 
+    //pages
+    Intent scanned_aps_page;
+    private Intent would_you_page;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // LayoutInflater inflater = LayoutInflater.from(); // or use your activity context
+       // View would_you_like_to_scan_layout = inflater.inflate(would_you_like_to_scan, null);
 
         image_view = findViewById(R.id.imageView);
         View imagePicker_button = findViewById(R.id.upload_map_button);
@@ -30,11 +41,16 @@ public class MainActivity extends AppCompatActivity {
         _wardriving_button = findViewById(R.id.wardriving_button);
         localization = findViewById(R.id.localization_button);
 
-        ShowDialog = findViewById(R.id.wardriving_button);
-        dialog = new Dialog(this);
-        dialog.setContentView(R.layout.pop_ip_dialog_screen);
+        //Dialogs
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.add_wifi_pop_up);
         dialog.setCancelable(true); //Optional
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
+
+//        wold_you_lie_to_scan = new Dialog(this);
+//        wold_you_lie_to_scan.setContentView(would_you_like_to_scan);
+//        wold_you_lie_to_scan.setCancelable(true); //Optional
+//        wold_you_lie_to_scan.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
 
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
                 registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
@@ -62,6 +78,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Scanning wifi
+        scanned_aps_page = new Intent(MainActivity.this, Scanned_Aps.class);
+        would_you_page = new Intent(MainActivity.this, popUp_would_you_like.class);
+
         delete_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,15 +94,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-        ShowDialog.setOnClickListener(new View.OnClickListener() {
+        View inflatedView = getLayoutInflater().inflate(would_you_like_to_scan, null);
+        View scan_yes = inflatedView.findViewById(R.id.scan_yes);
+        scan_yes.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                dialog.show(); // Showing the dialog here
+            public void onClick(View view) {
+                startActivity(scanned_aps_page);
             }
         });
 
-    }
-}
+        _wardriving_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(scanned_aps_page); // Showing the dialog here
+            }
+        });
+
+       image_view.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+                dialog.show();
+           }
+       });
+       localization.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(scanned_aps_page);
+
+           }
+       });
+}}
---
+++ app/src/main/java/com/ghayrat_samiev/mobile_com/popUp_would_you_like.java
@@ -0,0 +1,13 @@
+package com.ghayrat_samiev.mobile_com;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class popUp_would_you_like extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.would_you_like_to_scan);
+    }
+}
